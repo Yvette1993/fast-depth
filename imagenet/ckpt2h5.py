50ddeaf7a67819c3c094a66fd29affcf164e5c74
lisali <yingyinglee1993@163.com>
2019-08-02 20:54:06 +0800

    add

---
+++ imagenet/ckpt2h5.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import deepdish as dd
+import argparse
+import os
+import numpy as np
+from tensorflow.python import pywrap_tensorflow
+
+def tr(v):
+    # tensorflow weights to pytorch weights
+    if v.ndim == 4:
+        return np.ascontiguousarray(v.transpose(3,2,0,1))
+    elif v.ndim == 2:
+        return np.ascontiguousarray(v.transpose())
+    return v
+
+def read_ckpt(ckpt):
+    # https://github.com/tensorflow/tensorflow/issues/1823
+    reader = tf.train.NewCheckpointReader(ckpt)
+    
+    #reader = pywrap_tensorflow.NewCheckpointReader(ckpt)
+    #var_dict = reader.get_variable_to_shape_mao()
+    #for key in var_dict:
+    #    print("variable name:", key)
+    #    print(reader.get_tensor(key))
+
+    weights = {n: reader.get_tensor(n) for (n, _) in reader.get_variable_to_shape_map().items()}
+    pyweights = {k: tr(v) for (k, v) in weights.items()}
+    return pyweights
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Converts ckpt weights to deepdish hdf5")
+    parser.add_argument("infile", type=str,
+                        help="Path to the ckpt.")  # ***model.ckpt-22177***
+    parser.add_argument("outfile", type=str, nargs='?', default='',
+                        help="Output file (inferred if missing).")
+    args = parser.parse_args()
+    if args.outfile == '':
+        args.outfile = os.path.splitext(args.infile)[0] + '.h5'
+    outdir = os.path.dirname(args.outfile)
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    #read_ckpt(args.infile)
+
+    weights = read_ckpt(args.infile)
+    dd.io.save(args.outfile, weights)
+    weights2 = dd.io.load(args.outfile)
